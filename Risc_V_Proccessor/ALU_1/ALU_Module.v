784f9c11e1bccc342adc03140e3a2842285bb780
Hkmm07 <mcardleconor.work@gmail.com>
2026-06-18 01:58:04 +0100

    Updated ADD & SUB, Added negative Carry and Overflow flags, Yet to finish testbench

--- Risc_V_Proccessor/ALU_1/ALU_Module.v
+++ Risc_V_Proccessor/ALU_1/ALU_Module.v
@@ -5,20 +5,40 @@ module alu(
     input [31:0] b,
     input [2:0] select,
     output reg [31:0] result,
-    output zero                                             // It seems to be refered to as "zero flag" in most examples
+    output zero,                                            // It seems to be refered to as "zero flag" in most examples
+    output reg negative,
+    output reg carry,
+    output reg overflow
 );
 
+    reg [32:0] temporary;
+
 always@(*) begin
+    carry = 1'b0;                                           // Carry by default set to 0
+    overflow = 1'b0;                                        // Overflow by default set to 0
+
     case(select)
-    3'b000: result = a + b;                                 // Add
-    3'b001: result = a - b;                                 // Sub
+    3'b000: begin                                           // Add
+        temporary = {1'b0, a} + {1'b0, b};
+        result = temporary[31:0];
+        carry = temporary[32];
+        overflow = (a[31] == b [31]) && (result[31] != a[31]);
+    end
+    3'b001: begin                                           // Sub
+        temporary = {1'b0, a} - {1'b0, b};
+        result = temporary[31:0];
+        carry = temporary[32];
+        overflow = (a[31] != b[31]) && (result[31] != a[31]);
+    end
     3'b010: result = a & b;                                 // And
     3'b011: result = a ^ b;                                 // Or
     3'b100: result = a | b;                                 // Xor
-    3'b101: result = a << b[4:0];                           // SLL    (a^b)
+    3'b101: result = a << b[4:0];                           // SLL    (a to the power b)
     3'b110: result = ($signed(a)) < ($signed(b));           // SLT    $signed used so that negative numbers dont result in errors
     default: result = 32'b0;
     endcase
+
+    negative = result[31];
 end
 
 assign zero = (result == 32'b0);                            // Zero Flag
--- Risc_V_Proccessor/ALU_1/ALU_Module_Tb.v
+++ Risc_V_Proccessor/ALU_1/ALU_Module_Tb.v
@@ -3,25 +3,72 @@
 module alu_tb;
 
 reg [31:0] a, b;
-reg [2:0] select;
-reg [31:0] expected_result;
+reg [2:0] sel;
+reg [31:0] e_result;
 wire [31:0] result;
+reg e_zero, e_negative, e_carry, e_overflow;
+wire zero, neg, carry, overflow;
 
-alu uut (.a(a), .b(b), .result(result));
+
+alu uut (.a(a), .b(b), .select(sel), .result(result), .zero(zero), .negative(neg), .carry(carry), .overflow(overflow));
 
 initial begin
     $dumpfile("dump.vcd");
     $dumpvars(0, alu_tb);
 
     // Add tests
-
-a = 5; b = 10; expected_result = 15; select = 3'd000; #10;
-if (result == expected_result)
+    // 5 + 10 = 15
+a = 5; b = 10; e_result = 15; sel = 3'b000; #10;
+if (result == e_result)
     $display("Pass");
 else
-    $display("Fail: %0d (expected %0d)", result, expected_result);
+    $display("Fail: %0d (expected %0d)", result, e_result);
 
+    // 7 + (-7) = 0
+a = 100; b = 321; e_result = 421; sel = 3'b000; #10;
+if (result == e_result)
+    $display("Pass");
+else
+    $display("Fail: %0d (expected %0d)", result, e_result);
 
+    // 0 + 0 = 0
+a = 0; b = 0; e_result = 0; sel = 3'b000; #10;
+if (result == e_result)
+    $display("Pass");
+else
+    $display("Fail: %0d (expected %0d)", result, e_result);
+
+    // Sub tests
+    // 15 - 10 = 5
+a = 15; b = 10; e_result = 5; sel = 3'b001; #10;
+if (result == e_result)
+    $display("Pass");
+else
+    $display("Fail: %0d (expected %0d)", result, e_result);
+
+    // 7 - (-7) = 0
+a = 7; b = -7; e_result = 0; sel = 3'b001; #10;
+if (result == e_result)
+    $display("Pass");
+else
+    $display("Fail: %0d (expected %0d)", result, e_result);
+
+    // 0 - 0 = 0
+a = 0; b = 0; e_result = 0; sel = 3'b001; #10;
+if (result == e_result)
+    $display("Pass");
+else
+    $display("Fail: %0d (expected %0d)", result, e_result);
+
+    // And tests
+    // 010 & 110 = 010
+a = 32'b010; b = 32'b110; e_result = 32'b010; sel = 3'b010; #10;
+if (result == e_result)
+    $display("Pass");
+else
+    $display("Fail: %0d (expected %0d)", result, e_result);
+
+    // 
 $finish;
 end
 endmodule
