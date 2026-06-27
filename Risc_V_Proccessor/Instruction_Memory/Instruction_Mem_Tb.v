85abc679232fa05f2cee19e39b062fd3f848c878
Hkmm07 <mcardleconor.work@gmail.com>
2026-06-27 16:15:26 +0100

    Instruction Memory Module finished, testbench and ample hex file fineshed too

--- Risc_V_Proccessor/Instruction_Memory/Instruction_Mem_Tb.v
+++ Risc_V_Proccessor/Instruction_Memory/Instruction_Mem_Tb.v
@@ -12,7 +12,29 @@ initial begin
     $dumpfile("dump.vcd");
     $dumpvars(0, instruction_mem_tb);
 
+// Test second instruction
 pc = 32'd0; e_instruction = 32'h00500113; #10;
+if (e_instruction == instruction)
+    $display("Pass");
+else
+    $display("Fail! Result/Expected - Instruction:%0d/%0d", instruction, e_instruction);
+
+// Test Second Instruction
+pc = 32'd4; e_instruction = 32'h00A00193; #10;
+if (e_instruction == instruction)
+    $display("Pass");
+else
+    $display("Fail! Result/Expected - Instruction:%0d/%0d", instruction, e_instruction);
+
+// Test third instruction
+pc = 32'd8; e_instruction = 32'h002081B3; #10;
+if (e_instruction == instruction)
+    $display("Pass");
+else
+    $display("Fail! Result/Expected - Instruction:%0d/%0d", instruction, e_instruction);
+
+// Test Jump (jump to 8th line)
+pc = 32'd28; e_instruction = 32'h00419433; #10;
 if (e_instruction == instruction)
     $display("Pass");
 else
